12d7f7434e40a9791ce9ac859d8fbcccffe31e83
Adonis Izaguirre <94752464+AdonsIzaguirre@users.noreply.github.com>
2023-03-22 16:40:46 -0600

    Add files via upload

--- core/core.py
+++ core/core.py
@@ -51,6 +51,8 @@ Optional args:
 	--shell      Select interpreter [cmd, sh, bash, /bin/bash, /bin/sh, powershell, etc]
 	--payload    Select payload type [all, python, socket, rvim, vim, openssl, rview, view, ksh, gimp, gdb, irb, cpan, easyinstall, bash, netcat, c, c#, haskell, perl, php, ruby, socat, nodejs, java, javascript, telnet, zsh, lua, golang, vlang, awk, dart]
 	--os         Select target arc [linux, windows, mac]
+	--lhost      Set attacker IP
+	--lport      Set attacker port
 
 Examples:
 	python3 Moonveil.py reverse --os linux --payload php --shell sh
